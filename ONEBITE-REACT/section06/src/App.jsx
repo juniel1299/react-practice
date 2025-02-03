e91a5dbfd10dbe7460bf4bd5dcc742c6fe1be168
juniel1299 <youngs5440@gmail.com>
2025-02-03 16:13:40 +0900

    section06 카운터앱

--- ONEBITE-REACT/section06/src/App.jsx
+++ ONEBITE-REACT/section06/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Viewer from './components/Viewer';
+import Controller from './components/Controller';
+import {useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+// 현재 controller ,  viewer 가 부모 자식 관계가 아닌 동일 선상에 있으므로 props 가 안 됨 
+// 리액트에서 데이터의 흐름은 부모에서 자식 방향으로만 갈 수 있다. (단방향 데이터 흐름)
+// 즉 App 에서 Viewer , Controller 내용을 넘겨줘야함 
+// State Lifting (State 끌어 올리기)
+function App(){
+  // count를 부모인 app.jsx 에 생성해서 viewer에 넘겨주자 . 
+  const [count , setCount] = useState(0);
+
+  // 결국 count , setCount 모두 알아야 Controller 가 현재값을 기준으로 계산하므로 이벤트 핸들러 또한 App.jsx 에 만든다 . 
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>
+        Simple Counter
+      </h1>
+      <section>
+        <Viewer count = {count}/>  
+      </section>
+      <section>
+        <Controller onClickButton = {onClickButton}/>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
