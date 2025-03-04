56125dc246eeb6abbf2adc42a1ce1261d45af612
juniel1299 <youngs5440@gmail.com>
2025-03-04 13:22:59 +0900

    이미지 최적화 및 SEO

--- ONEBITE-NEXT/section08/src/app/@modal/default.tsx
+++ ONEBITE-NEXT/section08/src/app/@modal/default.tsx
@@ -1,3 +1,4 @@
 export default function Default(){
+    // 아무것도 없어서 페이지 컴포넌트 반환
     return null;
 }
---
+++ ONEBITE-NEXT/section09/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,58 @@
+import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+async function SearchResult({ q }: { q: string }) {
+  await delay(1500);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    { cache: "force-cache" }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+export async function generateMetadata( { searchParams }
+  : {searchParams: Promise<{ q: string }>}
+) : Promise<Metadata> {
+  const {q} = await searchParams;
+  return{
+    title: `${q} : 한입북스 검색`,
+    description : `${q}의 검색 결과`,
+    openGraph : {
+      title : `${q}의 검색 결과입니다.`,
+      description : `${q}의 검색 결과`,
+      images : ["/thumbnail.png"],
+    },
+  }
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <Suspense
+      key={searchParams.q || ""}
+      fallback={<BookListSkeleton count={3} />}
+    >
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
+  );
+}
